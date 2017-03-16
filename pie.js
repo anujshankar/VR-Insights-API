abe1b363c56534bad4017009d41c80584f28809d
Ar11rA <aritra.ghosh2295@yahoo.com>
2017-03-16 21:37:30 +0530

    Added labels to the pie chart

--- pie.js
+++ pie.js
@@ -3,27 +3,41 @@ var Canvas = require('canvas')
   , ctx = canvas.getContext('2d')
   , Chart = require('nchart')
   , fs = require('fs')
-
-new Chart(ctx).Pie(
-  [
-    {
-      'value': 10
-      , 'color': '#E2EAE9'
-    }
-    , {
-      'value': 100
-      , 'color': '#D4CCC5'
-    }
-    , {
-      'value': 40
-      , 'color': '#949FB1'
-    }
-  ]
-  , {
-    scaleShowValues: true
-    , scaleFontSize: 24
+const data = [
+  {
+    value: 100,
+    color: '#F7464A',
+    highlight: '#FF5A5E',
+    label: 'Red',
+    labelColor: 'white',
+    labelFontSize: '16'
+  },
+  {
+    value: 50,
+    color: '#46BFBD',
+    highlight: '#5AD3D1',
+    label: 'Green1',
+    labelColor: 'white',
+    labelFontSize: '16'
+  },
+  {
+    value: 100,
+    color: '#FDB452',
+    highlight: '#FFC870',
+    label: 'Yellow',
+    labelColor: 'white',
+    labelFontSize: '16'
   }
-)
+]
+var optionsPie = {
+  tooltipEvents: [],
+  showTooltips: true,
+  onAnimationComplete: function () {
+    this.showTooltip(this.segments, true)
+  },
+  tooltipTemplate: '<%= label %> - <%= value %>'
+}
+new Chart(ctx).Pie(data, optionsPie)
 
 canvas.toBuffer(function (err, buf) {
   if (err) throw err
